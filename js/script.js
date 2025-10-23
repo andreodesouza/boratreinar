cb2fa26683408b33dfe096e867563099041cd191
André <andreodesouza@outlook.com.br>
2025-10-23 16:02:54 -0300

    Alteração de Imagem

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 document.getElementById("btnLogin").addEventListener("click", function() {
   document.getElementById("logo").style.width = "150px";
+  document.body.style.background = "url('img/telalogin.png')";
   document.getElementById("telaInicial").style.display = "none";
   document.getElementById("telaLogin").style.display = "block";
 });
