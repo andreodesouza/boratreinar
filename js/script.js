2b2422c752ed4fc44c82d13680d7f6530d98cf80
André <andreodesouza@outlook.com.br>
2025-10-28 13:40:21 -0300

    Nova tela de registro

--- js/script.js
+++ js/script.js
@@ -14,6 +14,22 @@ document.getElementById("btnLogin").addEventListener("click", function() {
   document.getElementById("telaLogin").style.display = "flex";
 });
 
+document.getElementById("btnRegistrar").addEventListener("click", function(){
+document.getElementById("logo").style.width = "150px";
+document.body.style.backgroundImage = "url('img/telalogin.png')"
+document.body.style.backgroundSize = "cover";
+document.body.style.backgroundPosition = "center";
+document.body.style.backgroundRepeat = "no-repeat";
+document.body.style.height = "100vh";
+document.body.style.margin = "0";
+document.body.style.display = "flex";
+document.body.style.justifyContent = "center";
+document.body.style.alignItems = "center";
+document.getElementById("telaInicial").style.display = "none";
+document.getElementById("telaregister").style.display = "flex";  
+
+})
+
 document.getElementById("btnVoltar").addEventListener('click', function(){
   document.getElementById("telaLogin").style.display = "none";
   document.getElementById("telaInicial").style.display = "flex";
@@ -21,6 +37,13 @@ document.getElementById("btnVoltar").addEventListener('click', function(){
     document.getElementById("logo").style.width = "300px";
 
 });
+
+document.getElementById("btnvoltar2").addEventListener('click', function(){
+  document.getElementById("telaregister").style.display = "none";
+  document.getElementById("telaInicial").style.display = "flex";
+  document.body.style.backgroundImage = "url('img/fundo.png')";
+  document.getElementById("logo").style.width = "300px";
+})
 
 function login(){
   window.location.href = "home.html"
