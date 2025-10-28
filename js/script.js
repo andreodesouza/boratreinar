c308b975388d296497eb8a0b3f557f9a69fe1bd6
André <andreodesouza@outlook.com.br>
2025-10-28 15:13:33 -0300

    Nova Tela

--- js/script.js
+++ js/script.js
@@ -28,7 +28,7 @@ document.body.style.alignItems = "center";
 document.getElementById("telaInicial").style.display = "none";
 document.getElementById("telaregister").style.display = "flex";  
 
-})
+});
 
 document.getElementById("btnVoltar").addEventListener('click', function(){
   document.getElementById("telaLogin").style.display = "none";
@@ -38,12 +38,17 @@ document.getElementById("btnVoltar").addEventListener('click', function(){
 
 });
 
+document.getElementById("btncontinuar").addEventListener('click', function(){
+  document.getElementById("telaregister").style.display = "none";
+  document.getElementById("dados").style.display = "flex";
+});
+
 document.getElementById("btnvoltar2").addEventListener('click', function(){
   document.getElementById("telaregister").style.display = "none";
   document.getElementById("telaInicial").style.display = "flex";
   document.body.style.backgroundImage = "url('img/fundo.png')";
   document.getElementById("logo").style.width = "300px";
-})
+});
 
 function login(){
   window.location.href = "home.html"
