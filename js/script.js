67a00a568099db344489b40723876031a6dd738b
André <andreodesouza@outlook.com.br>
2025-10-31 12:56:08 -0300

    nova tela de treino

--- js/script.js
+++ js/script.js
@@ -34,7 +34,7 @@ document.getElementById("btnVoltar").addEventListener('click', function(){
   document.getElementById("telaLogin").style.display = "none";
   document.getElementById("telaInicial").style.display = "flex";
   document.body.style.backgroundImage = "url('img/fundo.png')";
-    document.getElementById("logo").style.width = "300px";
+  document.getElementById("logo").style.width = "300px";
 
 });
 
